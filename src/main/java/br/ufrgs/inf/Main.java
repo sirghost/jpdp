80656d09bf25f70c877ef4d53b236becbb1cbe22
Joe Smith <username@email.com>
2014-10-08 15:45:57 -0300

    First commit

---
+++ src/main/java/br/ufrgs/inf/Main.java
@@ -0,0 +1,12 @@
+package br.ufrgs.inf;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
